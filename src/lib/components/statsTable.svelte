3c99c93141c401616874f9f8aaabfece6b888961
airplayr <gameundwatch@gmail.com>
2026-03-02 21:28:49 +0900

    スタッツに色文字とアイコン追加

--- src/lib/components/statsTable.svelte
+++ src/lib/components/statsTable.svelte
@@ -5,28 +5,28 @@
 
 <Table.Root>
 	<Table.Body>
-		<Table.Row>
-			<Table.Cell class="font-semibold">Luck</Table.Cell>
+		<Table.Row class="text-yellow-500">
+			<Table.Cell class="font-semibold"><span class="text-xl">🍀</span> Luck</Table.Cell>
 			<Table.Cell class="text-right">{luck}</Table.Cell>
 		</Table.Row>
-		<Table.Row>
-			<Table.Cell class="font-semibold">Strength</Table.Cell>
+		<Table.Row class="text-red-500">
+			<Table.Cell class="font-semibold"><span class="text-xl">💪</span> Strength</Table.Cell>
 			<Table.Cell class="text-right">{strength}</Table.Cell>
 		</Table.Row>
-		<Table.Row>
-			<Table.Cell class="font-semibold">Expertise</Table.Cell>
+		<Table.Row class="text-cyan-500">
+			<Table.Cell class="font-semibold"><span class="text-xl">🎓</span> Expertise</Table.Cell>
 			<Table.Cell class="text-right">{expertise}</Table.Cell>
 		</Table.Row>
-		<Table.Row>
-			<Table.Cell class="font-semibold">Attraction Rate</Table.Cell>
+		<Table.Row class="text-green-500">
+			<Table.Cell class="font-semibold"><span class="text-xl">🐡</span> Attraction Rate</Table.Cell>
 			<Table.Cell class="text-right">{Math.min(attractionRate, 100)}</Table.Cell>
 		</Table.Row>
-		<Table.Row>
-			<Table.Cell class="font-semibold">Big Catch Rate</Table.Cell>
+		<Table.Row class="text-orange-500">
+			<Table.Cell class="font-semibold"><span class="text-xl">🦈</span> Big Catch Rate</Table.Cell>
 			<Table.Cell class="text-right">{bigCatchRate}</Table.Cell>
 		</Table.Row>
-		<Table.Row>
-			<Table.Cell class="font-semibold">Max Weight</Table.Cell>
+		<Table.Row class="text-purple-500">
+			<Table.Cell class="font-semibold"><span class="text-xl">⚖️</span> Max Weight</Table.Cell>
 			<Table.Cell class="text-right">{maxWeight.toLocaleString()}</Table.Cell>
 		</Table.Row>
 	</Table.Body>
