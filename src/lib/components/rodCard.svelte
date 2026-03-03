8fd6e240920c04dd143b6822bda473de8935fc3c
airplayr <gameundwatch@gmail.com>
2026-03-03 15:14:15 +0900

    エンチャント部分のUI追加

--- src/lib/components/rodCard.svelte
+++ src/lib/components/rodCard.svelte
@@ -2,22 +2,41 @@
 	import * as Card from '$lib/components/ui/card';
 	import * as Select from '$lib/components/ui/select';
 
+	let selected_rod_enchant = $state('');
+	const rod_enchant_names = ['none', 'bbb', 'ccc'];
+
 	let { rod_names, selected_rod = $bindable() } = $props();
-	const trigger = $derived(rod_names.find((r: string) => r === selected_rod) ?? 'select a rod');
+	const rodTrigger = $derived(rod_names.find((r: string) => r === selected_rod) ?? 'select a rod');
+	const rodEnchantTrigger = $derived(
+		rod_enchant_names.find((r: string) => r === selected_rod_enchant) ?? 'select a enchant'
+	);
 </script>
 
 <Card.Root>
 	<Card.Header>
 		<h2 class="text-xl font-bold">🎣 Rod</h2>
 	</Card.Header>
-	<Card.Content>
+	<Card.Content class="grid grid-cols-1 gap-2">
+		<!-- Base Rod Stats -->
 		<Select.Root type="single" bind:value={selected_rod}>
 			<Select.Trigger class="w-full">
-				{trigger}
+				{rodTrigger}
 			</Select.Trigger>
 			<Select.Content>
 				{#each rod_names as rod_name (rod_name)}
 					<Select.Item value={rod_name}>{rod_name}</Select.Item>
+				{/each}
+			</Select.Content>
+		</Select.Root>
+		<h3 class="mt-2 font-bold">🔮Enchant</h3>
+		<!-- Enchants -->
+		<Select.Root type="single" bind:value={selected_rod_enchant}>
+			<Select.Trigger class="w-full">
+				{rodEnchantTrigger}
+			</Select.Trigger>
+			<Select.Content>
+				{#each rod_enchant_names as rod_enchant_name (rod_enchant_name)}
+					<Select.Item value={rod_enchant_name}>{rod_enchant_name}</Select.Item>
 				{/each}
 			</Select.Content>
 		</Select.Root>
